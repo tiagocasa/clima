16caa17c9144db2840e16289f575f26009b47fed
Tiago <tiagocasa@gmail.com>
2023-04-03 16:46:37 -0300

    Aula 141

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -19,7 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
